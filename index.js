a95bc2c9d9c1faccd925a05d537a496b3b334230
z424brave <kelly.damian@btinternet.com>
2022-03-16 10:29:23 +0000

    another change

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const os= require('os');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello Damian");
+    response.end("Hello again Damian");
 });
 
 const port = process.env.PORT || 1337;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
-console.log(`Node ${process.version} / ${os.platform} / ${os.type}/ ${os.version} / ${os.release}`);
+console.log(`Node ${process.version} / ${os.platform} / ${os.type} / ${os.version} / ${os.release}`);
